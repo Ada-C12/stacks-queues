c93a9a74166ff5676a942139b12e6e2b13262c48
Farah Davoodi <farahdavoodi@gmail.com>
2020-02-27 15:53:40 -0800

    initial fork and clone and class portion

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,12 +1,21 @@
 class Queue
 
   def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    @store = Array.new(10)
+    @front = @back = -1 # nothing has been set yet so both will be set to -1
   end
 
   def enqueue(element)
-    raise NotImplementedError, "Not yet implemented"
+    if @front == -1 && @back == -1
+      @front = 0
+      @back = 1
+    end
+    if @front == @back
+      # DECIDE HERE
+    end
+
+    @store[@back] = element
+    @back = (@back + 1) % @store.length # but what happens when it reaches the end?
   end
 
   def dequeue
