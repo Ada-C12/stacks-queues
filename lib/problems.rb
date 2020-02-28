d6bf938267d383b76660afb36f4cc8737c1c5549
Tiffany Chio <tiffany.chio0801@gmail.com>
2020-02-27 21:59:58 -0800

    completed

--- lib/problems.rb
+++ lib/problems.rb
@@ -1,13 +1,69 @@
 require_relative './stack.rb'
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n), push and pop should be O(1) operations since the linked list has both a head and tail pointer
+# Space Complexity: O(n) if the string is not balanced and comprised wholly of open brackets, also O(n) if it actually closes
 def balanced(string)
-  raise NotImplementedError, "Not implemented yet"
+  return false if string.length % 2 == 1
+  return true if string.length == 0
+  
+  stack = Stack.new
+  
+  string.each_char do |char|
+    case char
+    when "{"
+      stack.push("}")
+    when "["
+      stack.push("]")
+    when "("
+      stack.push(")")
+    when "}"
+      return false if stack.pop != "}"
+    when "]"
+      return false if stack.pop != "]"
+    when ")"
+      return false if stack.pop != ")"
+    else
+      return false
+    end
+  end
+  
+  return true if stack.empty?
 end
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(n)
 def evaluate_postfix(postfix_expression)
-  raise NotImplementedError, "Not implemented yet"
+  return nil if postfix_expression.length == 0
+  
+  stack = Stack.new
+  
+  postfix_expression.each_char do |char|
+    int_conversion = char.to_i
+    if int_conversion > 0
+      stack.push(int_conversion)
+    elsif int_conversion == 0 && char == "0"
+      stack.push(int_conversion)
+    else
+      postfix_operation(char, stack)
+    end
+  end
+  
+  return stack.pop
+end
+
+def postfix_operation(operator, stack)
+  operand_2 = stack.pop
+  operand_1 = stack.pop
+  
+  case operator
+  when "+"
+    stack.push(operand_1 + operand_2)
+  when "-"
+    stack.push(operand_1 - operand_2)
+  when "*"
+    stack.push(operand_1 * operand_2)
+  when "/"
+    raise ArgumentError, 'Cannot divide by 0' if (operand_1 == 0 || operand_2 == 0)
+    stack.push(operand_1 / operand_2)
+  end
 end
--- lib/queue.rb
+++ lib/queue.rb
@@ -1,30 +1,47 @@
+require_relative './linked_list'
+
 class Queue
-
+  
   def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    # @store = Array.new(10)
+    # -1 is a flag to just say that this queue is empty
+    # @front = @back = -1
+    
+    @store = LinkedList.new
   end
-
+  
   def enqueue(element)
-    raise NotImplementedError, "Not yet implemented"
+    # if @front == -1 
+    #   @front = 0
+    #   @back = 1
+    # end
+    
+    # if @front == @back
+    #   # decide
+    # end
+    
+    # @store[@back] = element
+    # @back = (@back + 1) % @store.length
+    
+    @store.add_last(element)
   end
-
-  def dequeue
-    raise NotImplementedError, "Not yet implemented"
+  
+  def dequeue 
+    @store.remove_first
   end
-
+  
   def front
     raise NotImplementedError, "Not yet implemented"
   end
-
+  
   def size
     raise NotImplementedError, "Not yet implemented"
   end
-
+  
   def empty?
-    raise NotImplementedError, "Not yet implemented"
+    @store.empty?
   end
-
+  
   def to_s
     return @store.to_s
   end
