07096cfce4de4845cb5621196237493a9b454d06
Eve Le <nguyenthao.lee@gmail.com>
2020-02-27 19:20:32 -0800

    Implemented queue

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,31 +1,47 @@
 class Queue
 
   def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    @store = Array.new(100)
+    @start = -1
+    @end = -1
   end
 
   def enqueue(element)
-    raise NotImplementedError, "Not yet implemented"
+    if @start == -1 && @end == -1
+      @start = 0
+      @end = 1
+    end
+    
+    raise StandardError, "Queue is full"if @start == @end
+
+    @store[@end] = element
+    @end = (@end + 1) % @store.size
+    @start = (@start + 1) % @store.size if @store[@start].nil?
   end
 
   def dequeue
-    raise NotImplementedError, "Not yet implemented"
+    element = nil
+    if !empty?()
+      element = @store[@start]
+      @store[@start] = nil
+      @start = (@start + 1) % @store.size
+    end
+    return element
   end
 
   def front
-    raise NotImplementedError, "Not yet implemented"
+    return @store[@start]
   end
 
   def size
-    raise NotImplementedError, "Not yet implemented"
+    return @store.compact.size
   end
 
   def empty?
-    raise NotImplementedError, "Not yet implemented"
+    return (size() == 0)
   end
 
   def to_s
-    return @store.to_s
+    return @store.compact.to_s
   end
 end
