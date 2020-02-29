91e44f907d74edab93b019191747d8ec21d8b560
Dianna Tingg <dtingg@gmail.com>
2020-02-29 10:43:00 -0800

    fixed printing queue with circular buffer

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,6 +1,6 @@
 class Queue
   def initialize
-    @store = Array.new(25)
+    @store = Array.new(10)
 
     @front = -1
     @back = -1
@@ -11,9 +11,32 @@ class Queue
     if @front == -1
       @front = 0
       @back = 0
+
     # Queue is full
     elsif @front == @back
-      pass
+      length = @store.length
+      new_array = Array.new(length * 2)
+
+      write = 0
+      read = @front
+
+      while read < length
+        new_array[write] = @store[read]
+        read += 1
+        write += 1
+      end
+
+      read = 0
+
+      while write < length
+        new_array[write] = @store[read]
+        read += 1
+        write += 1
+      end
+
+      @store = new_array
+      @front = 0
+      @back = length
     end
 
     @store[@back] = element
@@ -45,6 +68,12 @@ class Queue
   end
 
   def to_s
-    return @store[@front...@back].to_s
+    if @back >= @front
+      return @store[@front...@back].to_s
+    else
+      queue = @store[@front...-1]
+      queue << @store[0..@back]
+      return queue
+    end
   end
 end
