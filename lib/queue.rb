c0d5a7a601391291acf5724fd049495d2f04b49c
Julia A Kingrey <juliakingreyworks@gmail.com>
2020-02-29 12:02:46 -0800

    Pulled in test changes

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,15 +1,36 @@
 class Queue
 
   def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    # assume fixed array length is 10
+    @store = Array.new(10)
+    
+    # currently front and back are both at 'front' of the queue
+    @front = -1 
+    @back = -1
   end
 
   def enqueue(element)
-    raise NotImplementedError, "Not yet implemented"
+    # adding to an empty queue
+    if @front == -1 && @back == -1
+      @front = 0
+      @back = 1
+    end
+
+    # if the queue is full 
+    if @front == @back
+      # tests require that the queue be resized 
+    end
+
+    @store[@back] = element
+    # use mod to wrap around
+    @back = (@back + 1) % @store.length
+
+
   end
 
   def dequeue
+    # increase at the front
+    # check that the queue is empty when you reach the end
     raise NotImplementedError, "Not yet implemented"
   end
 
