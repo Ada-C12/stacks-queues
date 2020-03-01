6f4ab5c2cf59dad7051c0444b0a7499cc6b3e568
Hallie <idhallie@gmail.com>
2020-03-01 13:11:42 -0800

    can evaluate a postfix expression.

--- lib/problems.rb
+++ lib/problems.rb
@@ -1,7 +1,7 @@
 require_relative './stack.rb'
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n) - touches every element of the string once.
+# Space Complexity: O(n) - in worst case, we add the same number of characters in the string to the stack.
 def balanced(string)
   return true if string.empty?
   
@@ -27,8 +27,32 @@ end
 return s.empty?
 end
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(n)
 def evaluate_postfix(postfix_expression)
-  raise NotImplementedError, "Not implemented yet"
+  operation = {
+  "+" => true,
+  "-" => true,
+  "*" => true,
+  "/" => true }
+  
+  s = Stack.new
+  index = 0
+  result = 0
+  
+  while index < postfix_expression.length do
+    if operation[postfix_expression[index]]
+      second = s.pop
+      first = s.pop
+      
+      result = first.to_i.public_send(postfix_expression[index], second.to_i)
+      s.push(result)
+      index += 1
+    else
+      s.push(postfix_expression[index])
+      index += 1
+    end
+  end
+  
+  return result
 end
