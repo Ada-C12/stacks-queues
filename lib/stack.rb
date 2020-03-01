0ce544f0d507779019d3a61531bc6982fdaf96a3
Sam <samcoll@Samanthas-MBP.localdomain>
2020-03-01 12:48:20 -0800

    Wave 1 - Implement a Stack - tests pass

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,21 +1,25 @@
+require_relative './linked_list.rb'
+
 class Stack
   def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    @store = LinkedList.new
   end
-
+  
   def push(element)
-    raise NotImplementedError, "Not yet implemented"
+    @store.add_last(element)
   end
-
+  
   def pop
-    raise NotImplementedError, "Not yet implemented"
+    return nil if self.empty?
+    element = @store.remove_last
+    
+    return element
   end
-
+  
   def empty?
-    raise NotImplementedError, "Not yet implemented"
+    return @store.empty?
   end
-
+  
   def to_s
     return @store.to_s
   end
