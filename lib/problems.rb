840423328d0b7bc6ac427970c121e6b8f0c09023
Mira <calopter@gmail.com>
2020-03-01 14:36:36 -0800

    wave 3

--- lib/problems.rb
+++ lib/problems.rb
@@ -1,9 +1,20 @@
 require_relative './stack.rb'
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(n)
 def balanced(string)
-  raise NotImplementedError, "Not implemented yet"
+  table = { ')' => '(', '}' => '{', ']' => '[' }
+  stack = Stack.new
+
+  string.each_char do |char|
+    if table.has_value? char
+      stack.push char
+    else
+      return false unless stack.empty? || table[char] == stack.pop
+    end
+  end
+  
+  return stack.empty?
 end
 
 # Time Complexity: ?
