dede9ffb32b0517ea53c6fabffeee2536a5c0eae
Hallie <idhallie@gmail.com>
2020-02-29 16:14:21 -0800

    Passes stack tests for create, empty, push.

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,31 +1,50 @@
 class Queue
-
+  
   def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    @store = Array.new(10)
+    @front = -1
+    @back = -1
   end
-
+  
   def enqueue(element)
-    raise NotImplementedError, "Not yet implemented"
+    if @front == -1 && @back == -1
+      front = 0
+      back = 1
+    end
+    
+    if @front == @back
+      return
+    end
+    
+    @store[@back] = element
+    @back = (@back + 1) % @store.length
   end
-
+  
   def dequeue
     raise NotImplementedError, "Not yet implemented"
   end
-
+  
   def front
     raise NotImplementedError, "Not yet implemented"
   end
-
+  
   def size
     raise NotImplementedError, "Not yet implemented"
   end
-
+  
   def empty?
     raise NotImplementedError, "Not yet implemented"
   end
-
+  
   def to_s
-    return @store.to_s
+    values = []
+    
+    @store.each do |i|
+      if !i.nil?
+        values << i
+      end
+    end
+    
+    return values.to_s
   end
 end
--- test/queue_test.rb
+++ test/queue_test.rb
@@ -7,31 +7,29 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 describe "Test Queue Implementation" do
   it "creates a Queue" do
     q = Queue.new
-    q.class.must_equal Queue
+    _(q.class).must_equal Queue
   end
-
+  
   it "adds something to an empty Queue" do
-    skip
     q = Queue.new
     q.enqueue(10)
-    q.to_s.must_equal "[10]"
+    _(q.to_s).must_equal "[10]"
   end
-
+  
   it "adds multiple somethings to a Queue" do
-    skip
     q = Queue.new
     q.enqueue(10)
     q.enqueue(20)
     q.enqueue(30)
-    q.to_s.must_equal "[10, 20, 30]"
+    _(q.to_s).must_equal "[10, 20, 30]"
   end
-
+  
   it "starts the size of a Queue at 0" do
     skip
     q = Queue.new
     q.empty?.must_equal true
   end
-
+  
   it "removes something from the Queue" do
     skip
     q = Queue.new
@@ -40,7 +38,7 @@ describe "Test Queue Implementation" do
     removed.must_equal 5
     q.empty?.must_equal true
   end
-
+  
   it "removes the right something (LIFO)" do
     skip
     q = Queue.new
@@ -51,7 +49,7 @@ describe "Test Queue Implementation" do
     removed.must_equal 5
     q.to_s.must_equal "[3, 7]"
   end
-
+  
   it "properly adjusts the size with enqueueing and dequeueing" do
     skip
     q = Queue.new
@@ -63,7 +61,7 @@ describe "Test Queue Implementation" do
     q.dequeue
     q.empty?.must_equal true
   end
-
+  
   it "returns the front element in the Queue" do
     skip
     q = Queue.new
@@ -100,7 +98,7 @@ describe "Test Queue Implementation" do
     q.enqueue(200)
     q.enqueue(210)
     q.dequeue
-
+    
     expect(q.to_s).must_equal('[40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240]')
   end
 end
