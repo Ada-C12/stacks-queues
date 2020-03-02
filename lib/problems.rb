2e6b26d2c68cb259095da7784aad3b0c37226dd0
geomsb <cpgeorgina.sanchez@gmail.com>
2020-03-01 22:08:21 -0800

    queue and stack methods

--- lib/problems.rb
+++ lib/problems.rb
@@ -3,7 +3,34 @@ require_relative './stack.rb'
 # Time Complexity: ?
 # Space Complexity: ?
 def balanced(string)
-  raise NotImplementedError, "Not implemented yet"
+  return false if string.length % 2 != 0
+
+  open_par = "([{"
+  close_par = "}])"
+
+  stack = Stack.new
+  balanced = true
+
+  string.each_char do |char|
+    if open_par.include? char 
+      stack.push(char)
+    end
+
+    if close_par.include? char
+      last = stack.pop
+      if char == ")" && last == "("
+        balanced = true
+      elsif char == "]" && last == "["
+        balanced = true
+      elsif char == "}" && last == "{"
+        balanced = true
+      else
+        balanced = false
+      end
+    end
+  end
+
+  return balanced
 end
 
 # Time Complexity: ?
--- lib/queue.rb
+++ lib/queue.rb
@@ -1,16 +1,48 @@
+require_relative 'linked_list'
+
 class Queue
 
   def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    @store = Array.new(100)
+    @front = @back = -1
+    @counter = 0
   end
 
   def enqueue(element)
-    raise NotImplementedError, "Not yet implemented"
+    if @front == -1 && @back == -1
+      @front = 0
+      @back = 0
+    end
+
+    # if @front == @back
+    #   raise NoSpaceError
+    # end
+
+    @counter += 1
+    @store[@back] = element
+    @back = (@back + 1) % @store.length
   end
 
   def dequeue
-    raise NotImplementedError, "Not yet implemented"
+    #Decrease front
+    if @front == -1 
+      raise QuequeEmptyError
+    end
+
+    @counter -= 1
+    data = @store[@front] 
+    @store[@front] = nil
+
+    if @front == @back
+      @front = -1
+      @back = -1
+    elsif @front == (@store.length - 1)
+      @front = 0
+    else
+      @front = @front + 1
+    end
+
+    return data
   end
 
   def front
@@ -18,14 +50,16 @@ class Queue
   end
 
   def size
-    raise NotImplementedError, "Not yet implemented"
+    return @counter
   end
 
   def empty?
-    raise NotImplementedError, "Not yet implemented"
+    return @counter == 0
   end
 
   def to_s
-    return @store.to_s
+    string = "["
+    string += @store.filter {|i| i != nil}.join(", ")
+    return string + "]"
   end
 end
