b28e968703f97330492979a841f3b65d323f2ba4
Diana <dianah46@gmail.com>
2020-03-01 22:45:57 -0800

    working through balanced problem

--- test/queue_test.rb
+++ test/queue_test.rb
@@ -11,14 +11,12 @@ describe "Test Queue Implementation" do
   end
 
   it "adds something to an empty Queue" do
-    skip
     q = Queue.new
     q.enqueue(10)
     expect(q.to_s).must_equal "[10]"
   end
 
   it "adds multiple somethings to a Queue" do
-    skip
     q = Queue.new
     q.enqueue(10)
     q.enqueue(20)
@@ -27,13 +25,11 @@ describe "Test Queue Implementation" do
   end
 
   it "starts the size of a Queue at 0" do
-    skip
     q = Queue.new
     q.empty?.must_equal true
   end
 
   it "a Queue is empty after removing all the elements" do
-    skip
     q = Queue.new
     q.enqueue(5)
     q.enqueue(6)
@@ -43,7 +39,6 @@ describe "Test Queue Implementation" do
   end
 
   it "removes something from the Queue" do
-    skip
     q = Queue.new
     q.enqueue(5)
     removed = q.dequeue
@@ -52,7 +47,6 @@ describe "Test Queue Implementation" do
   end
 
   it "removes the right something (LIFO)" do
-    skip
     q = Queue.new
     q.enqueue(5)
     q.enqueue(3)
@@ -63,7 +57,6 @@ describe "Test Queue Implementation" do
   end
 
   it "properly adjusts the size with enqueueing and dequeueing" do
-    skip
     q = Queue.new
     q.empty?.must_equal true
     q.enqueue(-1)
@@ -75,7 +68,6 @@ describe "Test Queue Implementation" do
   end
 
   it "returns the front element in the Queue" do
-    skip
     q = Queue.new
     q.enqueue(40)
     q.enqueue(22)
@@ -101,7 +93,6 @@ describe "Test Queue Implementation" do
     q.enqueue(120)
     q.enqueue(130)
     q.enqueue(140)
-    q.enqueue(150)
     q.enqueue(150)
     q.enqueue(160)
     q.enqueue(170)
