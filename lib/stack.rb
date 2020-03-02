d21168361459fb3b7312612de5b12daae709cab4
ohcloud <cloudylopez@icloud.com>
2020-03-01 20:31:16 -0800

    some tests passing, unsure how to get them to go

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,21 +1,20 @@
 class Stack
   def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    @store = LinkedList.new
   end
-
+  
   def push(element)
-    raise NotImplementedError, "Not yet implemented"
+    return @store.add_last(element)
   end
-
+  
   def pop
-    raise NotImplementedError, "Not yet implemented"
+    return @store.remove_last()
   end
-
+  
   def empty?
-    raise NotImplementedError, "Not yet implemented"
+    return true if @store.get_last.nil?
   end
-
+  
   def to_s
     return @store.to_s
   end
