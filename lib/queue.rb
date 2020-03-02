ecc712ed2ce9ad560860fa9def975952d6c67f49
Sara Shah Baig <sarashahbaig@gmail.com>
2020-03-01 20:24:36 -0800

    completed stacks

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,28 +1,64 @@
+# Wave 2
+# require_relative './stack.rb'
+require "./linked_list"
+
 class Queue
 
   def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    # raise NotImplementedError, "Not yet implemented"
+    @store = Array.new
+    @front = @back = -1
+# @front = -1
+# @back = -1
   end
 
   def enqueue(element)
-    raise NotImplementedError, "Not yet implemented"
+    # raise NotImplementedError, "Not yet implemented"
+    if @front == -1 && @back == -1
+      @front = 0
+      @back = 1
+    #  @front = 0
+    #  @back = 1
+    end
+
+    if @front == @back
+      # raise ArgumentError.new("Error")
+    end
+
+    @store[@front] = element
+    @front = (@back + 1) % @store.length + 1
+    @back =  @front + 1 #(@back + 1) % @store.length + 1
+
   end
 
   def dequeue
-    raise NotImplementedError, "Not yet implemented"
+    # raise NotImplementedError, "Not yet implemented"
+    # if @front == -1 && @back == -1
+    #   @front = @back = 1
+    # end
+    
+    # if @front == @back
+    #   # raise ArgumentError.new("Error")
+    # end
+    # puts @store
+    # puts @front
+    # puts @back
+    return @store.delete_at(@back)
   end
 
   def front
-    raise NotImplementedError, "Not yet implemented"
+    # raise NotImplementedError, "Not yet implemented"
+    return @store[@front]
   end
 
   def size
-    raise NotImplementedError, "Not yet implemented"
+    # raise NotImplementedError, "Not yet implemented"
   end
 
   def empty?
-    raise NotImplementedError, "Not yet implemented"
+    # raise NotImplementedError, "Not yet implemented"
+    return true if @front == -1 && @back == -1
+
   end
 
   def to_s
--- lib/stack.rb
+++ lib/stack.rb
@@ -1,19 +1,27 @@
+# wave 1
+require "./linked_list"
+# require_relative './stack.rb'
+
 class Stack
   def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    @store = LinkedList.new
+    # @front = -1
+    # raise NotImplementedError, "Not yet implemented"
   end
 
   def push(element)
-    raise NotImplementedError, "Not yet implemented"
+    # raise NotImplementedError, "Not yet implemented"
+    return @store.add_last(element)
   end
 
   def pop
-    raise NotImplementedError, "Not yet implemented"
+    # raise NotImplementedError, "Not yet implemented"
+    return @store.remove_last()
   end
 
   def empty?
-    raise NotImplementedError, "Not yet implemented"
+    # raise NotImplementedError, "Not yet implemented"
+    return @store.empty?
   end
 
   def to_s
