574968796a52b63a6873ca227f9061eb838a9abb
stupendousC <CarolineWuKaplan@gmail.com>
2020-03-01 19:50:18 -0800

    queue passed all tests, after I changed something in the tests, not sure if I missed something or was legit typos.  Posted on slack #csfundamentals for clarification

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,32 +1,35 @@
 class Queue
 
   def initialize
-    @store = Array.new()
+    @store = Array.new(10)
     @front = @back = -1
   end
 
   def enqueue(element)
-    puts "\n#{self}"
-
     # if queue is full, either raise error OR make queue bigger, in this case the test wants the queue to incr size
 
     if @front == -1 && @back == -1
       # if empty queue
       @front = 0
       @back = 0
-    elsif (@front== 0 && @back == @store.length-1) 
-      # queue is full, @back is on index -1, so just add new one onto the end
-      @back += 1 
-    elsif (@back + 1 == @front) 
-      # queue is full, but @end has wrapped around so it's not a simple addition onto the end
-      temp = @store.slice![@front..@store.length-1]
-      
-      @back += 1
-      @store[@back] << element
-      
-      @store.concat(temp)
-      @front += 1
-      return
+    elsif ((@back + 1) % self.size) == @front 
+      # queue is full, double existing size
+      blankArray = Array.new(self.size)
+      if @back == self.size + 1
+        # simple addition onto the end
+        @back += 1
+        @store = @store.concat(blankArray)
+      else
+        # queue is full, but @end has wrapped around so it's not a simple addition onto the end
+        temp = @store.slice!(@front..@store.length-1)
+        
+        @back += 1
+        @store[@back] = element
+        
+        @store.concat(temp)
+        @front += 1
+        return
+      end
     else
       # queue has room
       # modulus allows wraparound if needed :-)
@@ -34,34 +37,19 @@ class Queue
     end
 
     @store[@back] = element 
-    
-    puts "\t->#{self}"
   end
 
   def dequeue
-    puts "\n#{self}"
-
-    # tests also require resizing of queue with every removal
     return nil if self.empty?
 
     garbage = @store[@front]
-    @front = (@front + 1) % @store.length
-    
+
     if (@front == @back)
-      #  if @front and @back are at the same index, then queue is empty, reset
+      #  if @front and @back are at the same index, then queue will be empty after dequeuing, reset
       @front = -1
       @back = -1
-      @store = []
-    elsif @back > @front
-      # 1 continuous train, no wraparound
-      @store = @store.slice![@front..@back]
     else
-      # @front has wrapped around 
-      front_half = @store.slice![@front..self.size - 1]
-      back_half = @store.slice![0..@back]
-      @store = front_half + back_half
-      @front = 0
-      @end = self.size - 1
+      @front = (@front + 1) % @store.length
     end
 
     return garbage
@@ -80,6 +68,21 @@ class Queue
   end
 
   def to_s
-    return @store.to_s
+
+    return "[]" if self.empty?
+    return "[#{@store[0]}]" if (@front == @back)
+
+    printout = "["
+    
+    currIndex = @front 
+    until currIndex == @back
+      printout += "#{@store[currIndex]}, "
+
+      # advance index for next loop
+      currIndex = (currIndex + 1) % self.size
+    end
+    
+    printout << "#{@store[@back]}]"
+    return printout
   end
 end
