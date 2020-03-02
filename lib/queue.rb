deeb5aff8f3ca6759a1dbb79460ae19ae622aca4
Sam <samcoll@Samanthas-MBP.localdomain>
2020-03-01 17:23:28 -0800

    Wave 2 Implement a Queue

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,31 +1,53 @@
 class Queue
-
   def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    @store = Array.new(50)
+    @front = @back = -1
   end
-
+  
   def enqueue(element)
-    raise NotImplementedError, "Not yet implemented"
+    # queue is empty
+    if @front == -1 && @back == -1
+      @front = 0
+      @back = 1
+    end
+    
+    # queue is full
+    if @front == @back
+      raise ArgumentError, "Queue is full"
+    end    
+    
+    @store[@back] = element
+    @back = (@back + 1) % @store.length
+    
+    if @store[@front].nil?
+      @front = (@front + 1) % @store.length
+    end
   end
-
+  
   def dequeue
-    raise NotImplementedError, "Not yet implemented"
+    if @front == -1
+      raise ArgumentError, "Queue is empty"
+    end  
+    
+    removed = @store[@front]
+    @store[@front] = nil
+    @front = (@front + 1) % @store.length  
+    return removed
   end
-
+  
   def front
-    raise NotImplementedError, "Not yet implemented"
+    return @store[@front]
   end
-
+  
   def size
-    raise NotImplementedError, "Not yet implemented"
+    return @store[@front...@back].length
   end
-
+  
   def empty?
-    raise NotImplementedError, "Not yet implemented"
-  end
-
+    return @front == -1
+  end 
+  
   def to_s
-    return @store.to_s
+    return @store[@front...@back].to_s
   end
-end
+end 
