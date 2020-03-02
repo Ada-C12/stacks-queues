da3e4cf0172a5ac7410e67891ab40f50e2a5abe1
morganschuler <morganlschuler@gmail.com>
2020-03-01 19:05:01 -0800

    Finished for waves 1 and 2

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,21 +1,23 @@
+require "linked_list.rb"
+
 class Stack
   def initialize
     # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    @store = LinkedList.new
   end
-
+  
   def push(element)
-    raise NotImplementedError, "Not yet implemented"
+    @store.add_last(element)
   end
-
+  
   def pop
-    raise NotImplementedError, "Not yet implemented"
+    @store.remove_last()
   end
-
+  
   def empty?
-    raise NotImplementedError, "Not yet implemented"
+    @store.empty?
   end
-
+  
   def to_s
     return @store.to_s
   end
