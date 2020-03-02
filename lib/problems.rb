5282c9eb840846b46162d409ce8a13ad54dc349c
Kelsey Krippaehne <kelseykripp@gmail.com>
2020-03-01 21:53:32 -0800

    Wave 3 (balanced)

--- lib/problems.rb
+++ lib/problems.rb
@@ -3,7 +3,43 @@ require_relative './stack.rb'
 # Time Complexity: ?
 # Space Complexity: ?
 def balanced(string)
-  raise NotImplementedError, "Not implemented yet"
+  
+  
+  # paren pairs are at the same indexes
+  open_parens = [ '(', '[', '{' ]
+  close_parens = [')', ']', '}' ]
+  
+  # check if string length isn't even
+  if string.length % 2 != 0
+    return false
+    # check if string is empty
+  elsif string.length == 0
+    return true
+  else
+    # check if string starts with a close paren or ends with an open paren (unbalanced)
+    if (close_parens.include? string[0]) || (open_parens.include? string[-1])
+      return false
+    else
+      stack = Stack.new
+    end
+  end  
+  
+  string.each_char do |x|
+    if open_parens.include? x
+      stack.push(x)
+      # if x is a close paren, remove last open paren in stack and check to ensure it's a match for the close paren x.
+    elsif close_parens.include? x
+      last_in_stack = stack.pop()
+      if last_in_stack != open_parens[close_parens.index(x)]
+        return false
+      end
+    end
+  end
+  if stack.empty?
+    return true
+  else 
+    return false
+  end
 end
 
 # Time Complexity: ?
