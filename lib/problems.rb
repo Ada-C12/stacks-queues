71de3f2da7efed65934ee9af4c143f8363879baf
Julia A Kingrey <juliakingreyworks@gmail.com>
2020-03-02 17:20:28 -0800

    Small edit to balanced problem space complexity

--- lib/problems.rb
+++ lib/problems.rb
@@ -1,7 +1,7 @@
 require_relative './stack.rb'
 
 # Time Complexity: O(2n) or just plain O(n) where n is length of the array because we are looping over the array twice: once when we split from string to array and a second time when we create the hash map. 
-# Space Complexity: constant because the number of variables stays the same no matter how long our string/array is
+# Space Complexity: constant because the number of variables stays (more or less) the same no matter how long our string/array is
 def balanced(string)
   if string.length == 0
     return true
