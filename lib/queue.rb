59b4fd93d56627cda8bceccee3ea29492c19c477
Eve Le <nguyenthao.lee@gmail.com>
2020-03-02 19:04:09 -0800

    Updated queue implementation and added test

--- lib/queue.rb
+++ lib/queue.rb
@@ -7,7 +7,13 @@ class Queue
   end
   
   def enqueue(element)
-    raise StandardError, "Queue is full" if @front == @back && !front.nil?
+    if (@front == @back) && !front.nil?
+      capacity = @store.size
+      @store = copy_content(Array.new(capacity*2)) 
+      @front = 0
+      @back = capacity
+    end
+
     @store[@back] = element
     @back = (@back + 1) % @store.size
   end
@@ -34,14 +40,20 @@ class Queue
   end
   
   def to_s
-    queue = []
+    queue = copy_content(Array.new(size()))
+    return queue.to_s
+  end
+
+  private
+  def copy_content(new_store)
     if front
       current_index = @front
-      while current_index != @back
-        queue << @store[current_index]
+      queue_size = size()
+      queue_size.times do |index|
+        new_store[index] = @store[current_index]
         current_index = (current_index + 1) % @store.size
       end
     end
-    return queue.to_s
+    return new_store
   end
 end
