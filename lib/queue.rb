0e89da568418be3eb2087bca40ccfc97f0e24179
Brianna Kemp <kemp.bri@gmail.com>
2020-03-02 21:56:21 -0800

    Wave 1 complete

--- lib/queue.rb
+++ lib/queue.rb
@@ -4,16 +4,14 @@ class Queue
     @store = Array.new(50)
     @front = -1
     @back = -1
-    raise NotImplementedError, "Not yet implemented"
   end
   
   def enqueue(element)
-    if @front == -1 && @back == -1
+    if((@front == 0 && @back == @store.length - 1) || (@front == @back + 1))
+      raise ArgumentError
+    elsif @front == -1 && @back == -1
       @front = 0
-      @back = 1
-    end
-    if @front == @back
-      #Decide
+      @back = 0
     end
     
     @store[@back] = element
@@ -21,22 +19,54 @@ class Queue
   end
   
   def dequeue
-    raise NotImplementedError, "Not yet implemented"
+    if @front == -1
+      raise ArgumentError
+    end
+    
+    data = @store[@front]
+    @store[@front] = nil
+    @front = (@front + 1) % @store.length
+    
+    if @front == @back
+      @front = -1
+      @back = -1
+    end
+    
+    return data
   end
   
   def front
-    raise NotImplementedError, "Not yet implemented"
+    # raise NotImplementedError, "Not yet implemented"
   end
   
   def size
-    raise NotImplementedError, "Not yet implemented"
+    if self.empty?
+      raise ArgumentError
+    end
+    
+    return @back - @front
   end
   
   def empty?
-    raise NotImplementedError, "Not yet implemented"
+    if @front == -1 && @back == -1
+      return true
+    else
+      return false
+    end
   end
   
   def to_s
-    return @store.to_s
+    return @store[@front...@back].to_s
   end
 end
+
+q = Queue.new
+q.enqueue(0)
+# q.enqueue(1)
+# q.enqueue(2)
+# q.enqueue(3)
+p q
+# p q.size
+p q.dequeue
+# p q.size
+p q
