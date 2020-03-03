27172af6787378ad45a9362f202b497cdb4b0624
Julia Bouvier <juliabouv@gmail.com>
2020-03-02 16:38:50 -0800

    Implemented queue

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,31 +1,57 @@
 class Queue
 
   def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    @store = Array.new(30)
+    @front = @back = -1
   end
 
   def enqueue(element)
-    raise NotImplementedError, "Not yet implemented"
+    if @front == -1 && @back == -1
+      @front = 0
+      @back = 0
+    end
+
+    if ((@back + 1) % @store.length) == @front
+      raise ArgumentError.new "Queue is full"
+    end
+
+    @store[@back] = element
+    @back = (@back + 1) % @store.length
   end
 
   def dequeue
-    raise NotImplementedError, "Not yet implemented"
+    if @front == @back
+      raise ArgumentError.new "Queue is full"
+    end
+
+    temp = @store[@front]
+    @store[@front] = nil
+    @front = (@front + 1) % @store.length
+    return temp
   end
 
   def front
-    raise NotImplementedError, "Not yet implemented"
+    return @store[@front]
   end
 
   def size
-    raise NotImplementedError, "Not yet implemented"
+    if @back.length > @front.length
+      return @back.length - @front.length
+    else
+      return @front.length - @back.length
+    end
   end
 
   def empty?
-    raise NotImplementedError, "Not yet implemented"
+    @front == @back? true : false
   end
 
   def to_s
+    if @front < @back
+      @store = @store[@front...@back]
+    elsif @front != @back
+      @store = @store[@back..-1] + @store[@front...@back]
+    end
     return @store.to_s
   end
 end
--- test/queue_test.rb
+++ test/queue_test.rb
@@ -11,14 +11,12 @@ describe "Test Queue Implementation" do
   end
 
   it "adds something to an empty Queue" do
-    skip
     q = Queue.new
     q.enqueue(10)
     q.to_s.must_equal "[10]"
   end
 
   it "adds multiple somethings to a Queue" do
-    skip
     q = Queue.new
     q.enqueue(10)
     q.enqueue(20)
@@ -27,13 +25,11 @@ describe "Test Queue Implementation" do
   end
 
   it "starts the size of a Queue at 0" do
-    skip
     q = Queue.new
     q.empty?.must_equal true
   end
 
   it "removes something from the Queue" do
-    skip
     q = Queue.new
     q.enqueue(5)
     removed = q.dequeue
@@ -42,7 +38,6 @@ describe "Test Queue Implementation" do
   end
 
   it "removes the right something (LIFO)" do
-    skip
     q = Queue.new
     q.enqueue(5)
     q.enqueue(3)
@@ -53,7 +48,6 @@ describe "Test Queue Implementation" do
   end
 
   it "properly adjusts the size with enqueueing and dequeueing" do
-    skip
     q = Queue.new
     q.empty?.must_equal true
     q.enqueue(-1)
@@ -65,7 +59,6 @@ describe "Test Queue Implementation" do
   end
 
   it "returns the front element in the Queue" do
-    skip
     q = Queue.new
     q.enqueue(40)
     q.enqueue(22)
@@ -92,7 +85,6 @@ describe "Test Queue Implementation" do
     q.enqueue(130)
     q.enqueue(140)
     q.enqueue(150)
-    q.enqueue(150)
     q.enqueue(160)
     q.enqueue(170)
     q.enqueue(180)
@@ -101,6 +93,6 @@ describe "Test Queue Implementation" do
     q.enqueue(210)
     q.dequeue
 
-    expect(q.to_s).must_equal('[40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240]')
+    expect(q.to_s).must_equal('[40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210]')
   end
 end
