8d026e71a66c1189520f6db7bbde93e358fd644f
Julia Bouvier <juliabouv@gmail.com>
2020-03-02 19:22:46 -0800

    Implemented balanced method

--- lib/problems.rb
+++ lib/problems.rb
@@ -3,11 +3,39 @@ require_relative './stack.rb'
 # Time Complexity: ?
 # Space Complexity: ?
 def balanced(string)
-  raise NotImplementedError, "Not implemented yet"
+  stack = Stack.new
+  pairs = {
+    ')'=> '(',
+    '}'=> '{',
+    ']'=> '['
+  }
+
+  string.each_char do |char|
+    if pairs.values.include?(char)
+      stack.push(char)
+    else
+    return false if stack.empty?
+    removed = stack.pop
+    return false unless removed == pairs[char]
+    end
+  end
+
+  stack.empty? ? true : false
 end
 
 # Time Complexity: ?
 # Space Complexity: ?
 def evaluate_postfix(postfix_expression)
-  raise NotImplementedError, "Not implemented yet"
+  stack = Stack.new
+  operators = {
+    "*" => :*,
+    "/" =>  :/,
+    "+" =>  :+,
+    "-" => :-,
+    "%" => :%,
+  }
+
+  postfix_expression.each_char do |char|
+
+  end
 end
