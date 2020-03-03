12e7397b14d820e74c6207ed2416048e72686726
criacuervos <vanessareigosa@gmail.com>
2020-03-03 10:50:25 -0800

    Added initialize and enqueue methods

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,12 +1,27 @@
 class Queue
 
   def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    @store = Array.new(10)
+    @front = -1 
+    @back = -1
   end
 
   def enqueue(element)
-    raise NotImplementedError, "Not yet implemented"
+    #Check if queue is empty
+    if @front == -1 && @back == -1
+      @front = 0
+      @back = 1 
+    end 
+    #Check if queue is full
+    if @front == @back
+      #Decide what to do if circular buffer is full 
+    end 
+
+    @store(@back) = element 
+    @back += 1 
+
+    #Make it wrap around when it reaches the end 
+    @back = (@back + 1) % @store.length
   end
 
   def dequeue
