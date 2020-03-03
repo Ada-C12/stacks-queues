c9b1287b4243905dfa64ad2d8911f6f08b6a44cd
Kristy Hisaw <kristy.hisaw@gmail.com>
2020-03-02 17:56:15 -0800

    added balanced string method, tests passing

--- lib/problems.rb
+++ lib/problems.rb
@@ -3,7 +3,25 @@ require_relative './stack.rb'
 # Time Complexity: ?
 # Space Complexity: ?
 def balanced(string)
-  raise NotImplementedError, "Not implemented yet"
+  close = {
+    "(" => ")",
+    "[" => "]",
+    "{" => "}"
+  }
+
+  stack = Stack.new
+
+  string.each_char do |char|
+    if close.keys.include?(char)
+      stack.push(char)
+    elsif close.values.include?(char)
+      return false if stack.empty?
+      element = stack.pop
+      return false if char != close[element]
+    end
+  end
+
+  return stack.empty? 
 end
 
 # Time Complexity: ?
