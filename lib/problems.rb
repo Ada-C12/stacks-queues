c4d5eb195caf3ef949b1b13103279e15f9abcd2a
Ball <fasv@M-C02YN1MCJG5J.nordstrom.net>
2020-03-02 22:16:53 -0800

    balanced

--- lib/problems.rb
+++ lib/problems.rb
@@ -1,9 +1,28 @@
-require_relative './stack.rb'
+require_relative '../lib/stack'
 
 # Time Complexity: ?
 # Space Complexity: ?
 def balanced(string)
-  raise NotImplementedError, "Not implemented yet"
+  return false if string.length.odd?
+  closings = {
+    "[" => "]",
+    "(" => ")",
+    "{" => "}",
+  }
+  
+  stack = Stack.new
+
+  i = 0
+  until i > string.length - 1 do
+    if i < string.length / 2
+      stack.push(string[i])
+    else
+      removed = stack.pop
+      return false if closings[removed] != string[i]
+    end
+    i += 1
+  end
+  return true
 end
 
 # Time Complexity: ?
