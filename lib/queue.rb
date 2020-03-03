64b28e3185189db99bae046569151ec35165bc4d
Brianna Kemp <bri.kemp@sap.com>
2020-03-02 19:14:10 -0800

    Initial set up

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,30 +1,41 @@
 class Queue
-
+  
   def initialize
-    # @store = ...
+    @store = Array.new(50)
+    @front = -1
+    @back = -1
     raise NotImplementedError, "Not yet implemented"
   end
-
+  
   def enqueue(element)
-    raise NotImplementedError, "Not yet implemented"
+    if @front == -1 && @back == -1
+      @front = 0
+      @back = 1
+    end
+    if @front == @back
+      #Decide
+    end
+    
+    @store[@back] = element
+    @back = (@back + 1) % @store.length
   end
-
+  
   def dequeue
     raise NotImplementedError, "Not yet implemented"
   end
-
+  
   def front
     raise NotImplementedError, "Not yet implemented"
   end
-
+  
   def size
     raise NotImplementedError, "Not yet implemented"
   end
-
+  
   def empty?
     raise NotImplementedError, "Not yet implemented"
   end
-
+  
   def to_s
     return @store.to_s
   end
