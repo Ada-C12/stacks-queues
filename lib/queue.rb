08353eaf5c7b4aac946da5db347b5bdab2f36b55
Angele Zamarron <angelez@allenai.org>
2020-03-02 23:27:06 -0800

    queue in progress

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,12 +1,26 @@
 class Queue
 
   def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    # first in, first out, like a check-out line
+    @store = Array.new(20)
+    # pointers to @store nodes
+    @front = @back = -1
   end
 
   def enqueue(element)
-    raise NotImplementedError, "Not yet implemented"
+    # if empty then add element node to index 0 
+    if @front == -1 && @back == -1
+      @front = 0
+      @back = 0
+    end
+    # if queue is full
+    if @front == ((@back + 1) % @store.length) 
+      return nil
+    end
+
+    @store[@back] = element
+    @back = (@back + 1) % @store.length
+    return @store[@back]
   end
 
   def dequeue
@@ -26,6 +40,13 @@ class Queue
   end
 
   def to_s
+    # from front to end
+    if @front < @back
+      @store = @store[@front...@back]
+    # from back to end, then start to front
+    else
+      @store = @store[@back..-1] + @store[0...@front]
+    end
     return @store.to_s
   end
 end
