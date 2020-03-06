27b0bea68b40ee352cedc8fda2a4df956204a595
Elizabeth Northrop <elizabethjnorthrop@gmail.com>
2020-03-05 18:30:29 -0800

    Worked on queue and stack.

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,16 +1,37 @@
 class Queue
 
   def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    @store = Array.new(10)
+    @size = 10
+    @front = -1
+    @front = -1
   end
 
   def enqueue(element)
-    raise NotImplementedError, "Not yet implemented"
+    if @front == -1
+      @front = 0
+      @back = 1
+      @store[@front] = element 
+    elsif @front == @back
+      raise ArgumentError, "This queue is full"
+    else
+      @store[@back] = element
+      @back = (@back + 1) % @size
+    end
   end
 
   def dequeue
-    raise NotImplementedError, "Not yet implemented"
+    if @front == -1
+      raise ArgumentError, "This queue is empty"
+    else
+      value = @store[@front]
+      @front = (@front + 1) % @size
+      if @front == @back 
+        @front = -1
+        @back = -1
+      end
+      return value
+    end
   end
 
   def front
@@ -22,10 +43,16 @@ class Queue
   end
 
   def empty?
-    raise NotImplementedError, "Not yet implemented"
+    return @front == -1
   end
 
   def to_s
-    return @store.to_s
+    list = []
+    current = @front
+    until curent == @back
+      list << @store[current]
+      current = (current + 1) % @size
+    end
+    return list.to_s
   end
 end
