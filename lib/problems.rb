b798493f4954e0e3746a352e5d68d2e50e17c263
Bri Latimer <33140749+brilatimer@users.noreply.github.com>
2020-03-08 19:07:13 -0700

    Update problems.rb

--- lib/problems.rb
+++ lib/problems.rb
@@ -1,13 +1,46 @@
 require_relative './stack.rb'
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(n)
 def balanced(string)
-  raise NotImplementedError, "Not implemented yet"
+  # is it evenly divided by 2? If yes, then return false
+  # equal number of open parans and close parens
+  # make a new stack, make balanced = true
+  # go through each string using each_char
+  # if open parens, push to stack
+  # if close parens, check if stack has this match. If yes, pop off match in stack
+  # if at end, stack is empty, it is balanced. If parens remains, it is not balanced
+
+  if string.length % 2 == 1
+    return false 
+  end
+
+  stack = Stack.new
+
+  string.each_char do |char|
+    if char == '('
+      stack.push '('
+    elsif char == ')' && stack.pop != '('
+      return false 
+    end
+
+    if char == '{'
+      stack.push '}'
+    elsif char == '}' && stack.pop != '{'
+      return false 
+    end
+
+    if char == '['
+      stack.push '['
+    elsif char == ']' && stack.pop != '['
+      return false  
+    end
+  end
+  return true
 end
 
 # Time Complexity: ?
 # Space Complexity: ?
-def evaluate_postfix(postfix_expression)
-  raise NotImplementedError, "Not implemented yet"
-end
+# def evaluate_postfix(postfix_expression)
+#   raise NotImplementedError, "Not implemented yet"
+# end
