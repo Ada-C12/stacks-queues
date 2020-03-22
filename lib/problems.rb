5daf9f24a14617ee27a391b72d7ccd35936f1aa5
Ga-Young Jin <gyjinn@gmail.com>
2020-03-21 23:43:50 -0700

    finished balanced problem

--- lib/problems.rb
+++ lib/problems.rb
@@ -1,9 +1,35 @@
 require_relative './stack.rb'
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n) where n is length of string
+# Space Complexity: O(n) where n is length of string
 def balanced(string)
-  raise NotImplementedError, "Not implemented yet"
+  if string.empty?
+    return true
+  end
+  
+  bracket_stack = Stack.new
+  
+  pairs = {
+    "{" => "}",
+    "(" => ")",
+    "[" => "]"
+  }
+  
+  string.each_char do |char| # T: O(n) n = length of string
+    if pairs[char]
+      bracket_stack.push(char) # S: O(n) n = length of string
+    else 
+      if char != pairs[bracket_stack.pop]
+        return false
+      end
+    end
+  end
+  
+  if bracket_stack.empty?
+    return true
+  else 
+    return false
+  end
 end
 
 # Time Complexity: ?
