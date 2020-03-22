042e48eb3a30a49a4ad2e9a432d6ff06530f15eb
Ga-Young Jin <gyjinn@gmail.com>
2020-03-21 22:27:20 -0700

    finished queue

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,31 +1,72 @@
 class Queue
-
+  
   def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    @store = Array.new(100)
+    @front = -1
+    @back = -1
   end
-
+  
   def enqueue(element)
-    raise NotImplementedError, "Not yet implemented"
+    if (@front == 0 && @back == @store.length - 1) || (@back == ((@front - 1) % (@store.length - 1)))
+      raise ArgumentError, "Queue is full."
+    elsif (@front == -1)
+      @front = 0
+      @back = 0
+      @store[@back] = element
+    elsif (@back == @store.length - 1 && @front != 0)
+      @back = 0
+      @store[@rear] = element
+    else
+      @back = @back + 1
+      @store[@back] = element
+    end
   end
-
+  
   def dequeue
-    raise NotImplementedError, "Not yet implemented"
+    if (@front == -1)
+      raise ArgumentError, "Queue is empty"
+    end
+    
+    data = @store[@front]
+    @store[@front] = NotImplementedError
+    
+    if (@front == @back)
+      @front = -1
+      @back = -1
+    elsif (@front == (size - 1))
+      @front = 0
+    else
+      @front = @front + 1
+    end
+    
+    return data
   end
-
+  
   def front
-    raise NotImplementedError, "Not yet implemented"
+    return @store[@front]
   end
-
+  
   def size
-    raise NotImplementedError, "Not yet implemented"
+    return @store.length
   end
-
+  
   def empty?
-    raise NotImplementedError, "Not yet implemented"
+    # return @store.empty?
+    if @front == -1 && @back == -1
+      return true
+    else
+      return false
+    end
   end
-
+  
   def to_s
-    return @store.to_s
+    # return @store.to_s
+    to_print = []
+    pointer = @front
+    while pointer != @back + 1
+      to_print << @store[pointer]
+      pointer = (pointer + 1) % (size)
+    end
+    return to_print.to_s
   end
 end
